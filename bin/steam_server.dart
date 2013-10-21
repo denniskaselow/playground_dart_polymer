4cf452d5485fc80a84cc3b5532d166c89f6006c3
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2013-10-21 17:35:43 +0200

    refactoring

--- bin/steam_server.dart
+++ bin/steam_server.dart
@@ -7,11 +7,12 @@ main() {
   HttpServer.bind('127.0.0.1', 8080).then((server) {
     server.listen((request) {
       HttpResponse response = request.response;
-      response.headers.add("Access-Control-Allow-Origin", "*, ");
+      response.headers.add("Access-Control-Allow-Origin", "http://127.0.0.1:3030");
 
       Map<String, String> params = request.uri.queryParameters;
       if (params.containsKey('path')) {
-        scanDirectory(params['path']);
+        var path = Uri.decodeComponent(params['path']);
+        scanDirectory(path);
       } else if (params.containsKey('term')) {
         String term = params['term'];
         var result = new Map<String, String>();
@@ -29,36 +30,51 @@ main() {
 }
 
 void scanDirectory(String path) {
-  path = Uri.decodeComponent(path);
   path = '$path/SteamApps';
   FileSystemEntity.isDirectory(path).then((_) {
     var dir = new Directory(path);
-    dir.list(recursive: false, followLinks: false).listen((entity) {
-      if (entity is File && entity.path.endsWith('.acf')) {
-        var file = entity as File;
-        file.readAsString().then((content) {
-          content = content.replaceAllMapped(new RegExp(r'"(\t\t)(".*")(\s*)(["\}])'), (m) {
-            if (m[4] == '"') {
-              return '":${m[2]},${m[3]}${m[4]}';
-            }
-            return '":${m[2]}${m[3]}${m[4]}';
-          });
-          content = content.replaceAllMapped(new RegExp(r'^("[a-zA-Z]+")(\n)'), (m) => '${m[1]}:${m[2]}');
-          content = content.replaceAllMapped(new RegExp(r'(\t+"[a-zA-Z]+")(\n)'), (m) => '${m[1]}:${m[2]}');
-          content = content.replaceAllMapped(new RegExp(r'}(\s+)"'), (m) => '},${m[1]}"');
-          content = content.replaceAll('\\', '/');
-          content = '{$content}';
-          Map<String, dynamic> app = JSON.decode(content);
-          Map<String, dynamic> appState = app['AppState'];
-          String name = appState['UserConfig']['name'];
-          if (name == null) {
-            name = appState['InstallDir'];
-          }
-          if (name != null) {
-            games[appState['appid']] = name;
-          }
-        });
+    dir.exists().then((exists) {
+      if (exists) {
+        addGamesInDir(dir);
       }
-    }, onError: (_) {});
+    });
   }, onError: (_) {});
+}
+
+void addGamesInDir(dir) {
+  dir.list(recursive: false, followLinks: false).listen((entity) {
+    if (entity is File && entity.path.endsWith('.acf')) {
+      var file = entity as File;
+      file.readAsString().then((content) {
+        content = toJson(content);
+        Map<String, dynamic> appState = JSON.decode(content)['appstate'];
+        String name = appState['userconfig']['name'];
+        String id = appState['appid'];
+        if (name == null) {
+          name = appState['installdir'];
+        }
+        if (name == null) {
+          name = '$id (no name found)';
+        }
+        if (id != null) {
+          games[appState['appid']] = name;
+        }
+      });
+    }
+  }, onError: (_) {});
+}
+
+String toJson(String content) {
+  var result = content.replaceAllMapped(new RegExp(r'"(\t\t)(".*")(\s*)(["\}])'), (m) {
+    if (m[4] == '"') {
+      return '":${m[2]},${m[3]}${m[4]}';
+    }
+    return '":${m[2]}${m[3]}${m[4]}';
+  });
+  result = result.replaceAllMapped(new RegExp(r'^("[a-zA-Z]+")(\n)'), (m) => '${m[1]}:${m[2]}');
+  result = result.replaceAllMapped(new RegExp(r'(\t+"[a-zA-Z]+")(\n)'), (m) => '${m[1]}:${m[2]}');
+  result = result.replaceAllMapped(new RegExp(r'}(\s+)"'), (m) => '},${m[1]}"');
+  result = result.replaceAllMapped(new RegExp(r'("\w+")'), (m) => m[1].toLowerCase());
+  result = result.replaceAll('\\', '/');
+  return '{$result}';
 }
